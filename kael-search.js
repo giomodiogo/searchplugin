89fb3b384a9ec54e3c25819d025ea7358c376ee2
Kael Santini <kaelsantini@hotmail.com>
2012-08-06 21:08:17 -0300

    Initial commit of plugin. Not yet completed. DO NOT USE IN PRODUCTION

---
+++ kael-search.js
@@ -0,0 +1,158 @@
+/**
+
+	use:
+		jquery
+		jquery-ui
+		
+		slickgrid
+
+**/
+(function($) {
+	$.widget( "ui-helpers.kaelsearch", {
+
+		options: {
+			title: "Search",
+			disableInput: true,
+			cachedResults: [],
+			search: $.noop,
+			
+			buttonFinderOptions : { 
+				icons: {
+					primary: "ui-icon-search"
+				},
+				text: false
+			},
+			
+			buttonSearchOptions : {
+				icons: {
+					primary: "ui-icon-search"
+				},
+				text: false
+			},
+			
+			dialogOptions : {
+				modal: true,
+				autoOpen: false,
+				height: 430,
+				width: 500
+			},
+			
+			gridOptions : {
+				columns: [
+					{
+						id: "idPK",
+						field: "id",
+						name: "ID",
+						sortable: true,
+						resizable: true,
+						width: 25
+					},
+					{
+						id: "idLbl",
+						name: "Description",
+						field: "label",
+						sortable: true
+					}
+				],
+				enableCellNavigation: true,
+				//autoHeight: true,
+				forceFitColumns: true,
+				multiSelect: false
+			}
+			
+		},
+
+		_create: function() {
+			
+			// manipulate events on _create method 
+			
+			
+		},
+		
+		_init: function() {
+		
+			if (this.options.disableInput)
+				this.element.attr("readonly", "readonly").attr("disabled", "disabled");
+		
+			this._dialogButton = $("<button>").button(this.options.buttonFinderOptions);
+			this._dialogButton.insertAfter(this.element);
+			this._dialogButton.click($.proxy(this, "_dialogButtonClick"));
+			
+			this._dialog = $("<div>").attr("title", this.options.title);
+			
+			//this._container = $("<div>").addClass("ui-widget-content ui-corner-all").css("display", "inline-block").appendTo(this._dialog);
+			//this._container = $("<div>").appendTo(this._dialog);
+			this._container = this._dialog;
+			
+			this._termInput = $("<input>").attr("type", "text").addClass("text ui-widget-content ui-corner-all").css("width", "100%");
+			this._termInput.appendTo(this._container);
+			
+			this._searchButton = $("<button>").css({"position":"absolute", "top" : "10px", "right" : "15px"});
+			this._searchButton.button(this.options.buttonSearchOptions);
+			this._searchButton.appendTo(this._container);
+			this._searchButton.click($.proxy(this, "_searchButtonClick"));
+			
+			this.options.dialogOptions.buttons = {};
+			this.options.dialogOptions.buttons.OK = $.proxy(this, "_okButtonClick");
+			this._dialog.dialog(this.options.dialogOptions);
+			
+		},
+		
+		_dialogButtonClick: function() {
+			this._dialog.dialog("open");
+			this._configureGrid();
+		},
+		
+		_configureGrid: function() {
+			if (!this._slickGrid) {
+				
+				this._gridContainer = $("<div>").css("height", this.options.dialogOptions.height - 100).appendTo(this._container);
+				this._slickGrid = new Slick.Grid(this._gridContainer[0], [], this.options.gridOptions.columns, this.options.gridOptions);
+				this._slickGrid.setSelectionModel(new Slick.RowSelectionModel());
+				//
+				if (this.options.gridOptions.multiSelect) {
+					var checkboxSelector = new Slick.CheckboxSelectColumn({
+						cssClass: "slick-cell-checkboxsel"
+					});
+					this.options.gridOptions.columns.splice(0, 0 , checkboxSelector.getColumnDefinition());
+					this._slickGrid.setColumns(this.options.gridOptions.columns);
+					this._slickGrid.registerPlugin(checkboxSelector)
+				}
+				
+				
+			}
+		},
+		
+		_searchButtonClick: function(){
+			if (this.options.cachedResults.length > 0) {
+				this._updateGrid(this.options.cachedResults);
+			} else {
+				//this._trigger("search", [ this._updateGrid ]);
+				this.options.search(this._termInput.val(), $.proxy(this, "updateGrid"));
+			}
+		},
+		
+		_okButtonClick: function() {
+			var selectedRows = this._slickGrid.getSelectedRows();
+			var selectedItem = this._slickGrid.getDataItem(selectedRows[0]);
+			this.element.val(selectedItem.label);
+			this._dialog.dialog("close");
+		},
+		
+		updateGrid: function(data) {
+			this._slickGrid.invalidateAllRows();
+			this._slickGrid.setData(data);
+			this._slickGrid.render();
+		},
+		
+		// overriding factory method
+		_setOption: function( key, value ) {
+			$.Widget.prototype._setOption.apply( this, arguments );
+			switch( key ) {
+			case "clear":
+			   break;
+			}
+		}
+	});
+}(jQuery));
+
