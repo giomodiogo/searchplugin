66821e85feeadf8f5494dcc4798c73e980d54323
Kael Santini <kaelsantini@hotmail.com>
2012-08-27 14:23:31 -0300

    changed file names

--- gridproviders/jqgridprovider.js
+++ gridproviders/jqgridprovider.js
@@ -2,9 +2,14 @@
 	Implementation for jqGrid used in kael-search.js
 		
 	@author Kael Santini
-	@since 23/08/2012
+	@since 27/08/2012
 
 **/
 var jqGridProvider = function() {
-	//not yet
+
+	var $grid;
+	var _options;
+
+
+	
 };
